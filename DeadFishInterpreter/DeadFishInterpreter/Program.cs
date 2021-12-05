a8eea19733d9276c99ecfaad6c4866080638d58b
ronaldarndt <ronaldarndtf@gmail.com>
2021-12-05 03:33:31 -0300

    first commit

---
+++ DeadFishInterpreter/DeadFishInterpreter/Program.cs
@@ -0,0 +1,13 @@
+﻿using var fs = new FileStream(args[0], FileMode.Open);
+var op = 0;
+var n = 0;
+
+while ((op = fs.ReadByte()) != -1)
+{
+    if (op == 'i') n++;
+    else if (op == 'd') n--;
+    else if (op == 's') n *= 2;
+    else if (op == 'o') Console.Write(n);
+
+    if (n < 0 || n == 256) n = 0;
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# What?
+
+This is a monorepo for all of my current and future (stupid) esolang implementations
